b32941ad57b3348f347844b04d9522858209d977
BigBowen <15915841839@163.com>
2019-06-19 18:04:14 +0800

    初始化

---
+++ LoadImage/LoadImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadImage
+//
+//  Created by bowenlin on 2019/6/4.
+//  Copyright © 2019年 weiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadImage/LoadImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadImage
+//
+//  Created by bowenlin on 2019/6/4.
+//  Copyright © 2019年 weiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
